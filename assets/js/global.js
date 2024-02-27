b13f975b1e84537da2c65659e676efe5e9e07fee
Thiagobernardofernandes <bernardofernandesthiago@gmail.com>
2024-02-26 22:10:11 -0300

    termino da estilização header e footer

--- assets/js/global.js
+++ assets/js/global.js
@@ -1,19 +1,51 @@
-//Menu Lateral
-document.getElementById("iconeMenu").addEventListener("click", () => {
-  let navbar = document.getElementById("menu");
-  let blurBackground = document.getElementById("blurBackground");
-  let botaoMenu = document.getElementById("botaoMenu");
+//Menu Hamburguer
+// document.querySelector(".iconeMenuHamburguer").addEventListener("click", () => {
+//   let menu = document.getElementsByClassName("menuHamburguer")[0];
+//   let openMenu = document.getElementById("openMenuHamburguer");
+//   let closeMenu = document.getElementById("closeMenuHamburguer");
 
-  if (navbar.style.left === "0px") {
-    navbar.style.left = "-360px";
-    blurBackground.style.display = "none";
-    botaoMenu.src = "./assets/img/shared/menu.svg";
-  } else {
-    navbar.style.left = "0";
-    blurBackground.style.display = "block";
-    botaoMenu.src = "./assets/img/shared/exit.svg";
-  }
-});
+//   console.log("oi");
+
+//   if (menu.id === closeMenu.id) {
+//     console.log("O elemento possui o mesmo ID que closeMenuHamburguer");
+//     menu.setAttribute("id", "openMenuHamburguer");
+//   } else if (menu.id === openMenu) {
+//     console.log("O elemento NÃO possui o mesmo ID que closeMenuHamburguer");
+//     menu.removeAttribute("id");
+//   }
+// });
+
+// document.querySelector(".iconeMenuHamburguer").addEventListener("click", () => {
+//   let botaoMenu = document.querySelector(".iconeMenuHamburguer");
+//   let menu = document.querySelector(".menuHamburguer");
+
+//   let openMenu = document.getElementById("openMenuHamburguer");
+//   let closeMenu = document.getElementById("closeMenuHamburguer");
+
+//   if (menu.style.left !== "0px") {
+//     closeMenu.toggleAttribute("id");
+//     openMenu.toggleAttribute("id");
+//   } else {
+//     openMenu.toggleAttribute("id");
+//     closeMenu.toggleAttribute("id");
+//   }
+// });
+
+// document.getElementById("iconeMenu").addEventListener("click", () => {
+//   let navbar = document.getElementById("menu");
+//   let blurBackground = document.getElementById("blurBackground");
+//   let botaoMenu = document.getElementById("botaoMenu");
+
+//   if (navbar.style.left === "0px") {
+//     navbar.style.left = "-360px";
+//     blurBackground.style.display = "none";
+//     botaoMenu.src = "./assets/img/shared/menu.svg";
+//   } else {
+//     navbar.style.left = "0";
+//     blurBackground.style.display = "block";
+//     botaoMenu.src = "./assets/img/shared/exit.svg";
+//   }
+// });
 
 const templatePersonagens = document.getElementById("personagens");
 
