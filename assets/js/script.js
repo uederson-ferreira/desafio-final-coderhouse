3db3fb460ca38cc43fd3d134e766b51f203b65d6
Uederson <uedersonferreira@gmail.com>
2024-03-06 20:38:58 -0300

    Adequacoes da pagina de naves

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,64 +1,3 @@
-window.onload = function () {
-    // Aguardar 5 segundos (5000 milissegundos) antes de chamar mostrarPopup()
-    setTimeout(function () {
-        mostrarPopup();
-    }, 5000);
-};
-
-// Função para definir um cookie
-function setCookie(cname, cvalue, exdays) {
-    var d = new Date();
-    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
-    var expires = "expires=" + d.toUTCString();
-    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-}
-
-// Função para obter o valor de um cookie
-function getCookie(cname) {
-    var name = cname + "=";
-    var decodedCookie = decodeURIComponent(document.cookie);
-    var ca = decodedCookie.split(';');
-    for (var i = 0; i < ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0) == ' ') {
-            c = c.substring(1);
-        }
-        if (c.indexOf(name) == 0) {
-            return c.substring(name.length, c.length);
-        }
-    }
-    return "";
-}
-
-// Função para verificar se o popup já foi exibido
-function checkPopup() {
-    var popupShown = getCookie("popupShown");
-    if (!popupShown) {
-        mostrarPopup();
-        setCookie("popupShown", "true", 1);  // Define o cookie para indicar que o popup foi exibido
-    }
-}
-
-function mostrarPopup() {
-    document.getElementById('popup').style.display = 'block';
-    document.getElementById('overlay').style.display = 'block';
-}
-
-function fecharPopup() {
-    document.getElementById('popup').style.display = 'none';
-    document.getElementById('overlay').style.display = 'none';
-}
-
-function enviarFormulario() {
-    // Lógica de processamento do formulário
-
-    // Exemplo de exibição de mensagem
-    alert('Cadastro realizado com sucesso!');
-
-    // Fechar o popup após o cadastro
-    fecharPopup();
-}
-
 const queryString = window.location.search;
 
 fetch("https://swapi.dev/api/starships/" + queryString.replace("?", ""))
@@ -73,3 +12,63 @@ fetch("https://swapi.dev/api/starships/" + queryString.replace("?", ""))
     });
   });
 
+// window.onload = function () {
+//     // Aguardar 5 segundos (5000 milissegundos) antes de chamar mostrarPopup()
+//     setTimeout(function () {
+//         mostrarPopup();
+//     }, 5000);
+// };
+
+// // Função para definir um cookie
+// function setCookie(cname, cvalue, exdays) {
+//     var d = new Date();
+//     d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+//     var expires = "expires=" + d.toUTCString();
+//     document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+// }
+
+// // Função para obter o valor de um cookie
+// function getCookie(cname) {
+//     var name = cname + "=";
+//     var decodedCookie = decodeURIComponent(document.cookie);
+//     var ca = decodedCookie.split(';');
+//     for (var i = 0; i < ca.length; i++) {
+//         var c = ca[i];
+//         while (c.charAt(0) == ' ') {
+//             c = c.substring(1);
+//         }
+//         if (c.indexOf(name) == 0) {
+//             return c.substring(name.length, c.length);
+//         }
+//     }
+//     return "";
+// }
+
+// // Função para verificar se o popup já foi exibido
+// function checkPopup() {
+//     var popupShown = getCookie("popupShown");
+//     if (!popupShown) {
+//         mostrarPopup();
+//         setCookie("popupShown", "true", 1);  // Define o cookie para indicar que o popup foi exibido
+//     }
+// }
+
+// function mostrarPopup() {
+//     document.getElementById('popup').style.display = 'block';
+//     document.getElementById('overlay').style.display = 'block';
+// }
+
+// function fecharPopup() {
+//     document.getElementById('popup').style.display = 'none';
+//     document.getElementById('overlay').style.display = 'none';
+// }
+
+// function enviarFormulario() {
+//     // Lógica de processamento do formulário
+
+//     // Exemplo de exibição de mensagem
+//     alert('Cadastro realizado com sucesso!');
+
+//     // Fechar o popup após o cadastro
+//     fecharPopup();
+// }
